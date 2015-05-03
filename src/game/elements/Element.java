0b05aa29b389d511cad2ff7c2b8103282f80315e
PatrickHald <patrick.hald@gmail.com>
2015-05-03 12:08:07 +0200

    Animation in progress.

--- src/game/elements/Element.java
+++ src/game/elements/Element.java
@@ -2,84 +2,124 @@ package game.elements;
 import Level.*;
 import game.Physics.BoundingShape;
 
+import java.util.HashMap;
+import org.newdawn.slick.Animation;
+
 import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
 
 
 public abstract class Element extends LevelObject {
 	
-		protected boolean                   moving = false;
-		protected float                     accelerationSpeed = 1;
-		protected float                     decelerationSpeed = 1;
-		protected float                     maximumSpeed = 1;
-	    protected float x;
-	    protected float y;
-	    protected Image sprite;
- 
+	protected HashMap<Facing,Image>		sprites;
+	
+	protected HashMap<Facing,Animation>	movingAnimations;
+	protected long 						lastTimeMoved;
+	protected boolean                   moving = false;
+	protected float                     accelerationSpeed = 1;
+	protected float                     decelerationSpeed = 1;
+	protected float                     maximumSpeed = 1;
+	protected float x;
+	protected float y;
+	//protected Image sprite;
+	protected Facing facing;
+	
 	public Element(float x, float y) throws SlickException{
 		super(x,y);
+		
+		setSprite(new Image("data/img/placeholder_sprite.png"));
+		
+		//all elements face right by default
+		facing = Facing.RIGHT;
 		// ----- Placeholder for image -----
 		//sprite = new Image("data/img/placeholder_sprite.png");
 	}
-	public boolean isMoving(){
-	        return moving;
-	    }
-	 
-	    public void setMoving(boolean b){
-	        moving = b;
-	    }
-	 
-	    //move towards x_velocity = 0
-	    public void decelerate(int delta) {
-	        if(x_velocity > 0){
-	            x_velocity -= decelerationSpeed * delta;
-	            if(x_velocity < 0)
-	                x_velocity = 0;
-	        }else if(x_velocity < 0){
-	            x_velocity += decelerationSpeed * delta;
-	            if(x_velocity > 0)
-	                x_velocity = 0;
-	        }
-	    }
-	 
-	    public void jump(){
-	        if(onGround)
-	            y_velocity = -0.4f;
-	    }
-	 
-	    public void moveLeft(int delta){
-	        //if we aren't already moving at maximum speed
-	        if(x_velocity > -maximumSpeed){
-	            //accelerate
-	            x_velocity -= accelerationSpeed*delta;
-	            if(x_velocity < -maximumSpeed){
-	                //and if we exceed maximum speed, set it to maximum speed
-	                x_velocity = -maximumSpeed;
-	            }
-	        }
-	        moving = true;
-	    }
-	 
-	    public void moveRight(int delta){
-	        if(x_velocity < maximumSpeed){
-	            x_velocity += accelerationSpeed*delta;
-	            if(x_velocity > maximumSpeed){
-	                x_velocity = maximumSpeed;
-	            }
-	        }
-	        moving = true;
-	    }
-	 
-	   
 	
-   
-
- 
+	public enum Facing{
+		LEFT, RIGHT
+	}
+	
+	protected void setMovingAnimation(Image[] images, int frameDuration){
+		movingAnimations = new HashMap<Facing,Animation>();
+		
+		movingAnimations.put(Facing.RIGHT, new Animation(images,frameDuration));
+		
+		Animation facingLeftAnimation = new Animation();
+		for(Image i : images){
+			facingLeftAnimation.addFrame(i.getFlippedCopy(true, false), frameDuration);
+		}
+		movingAnimations.put(Facing.LEFT, facingLeftAnimation);
+		
+	}
+		  
+	protected void setSprite(Image i){
+		 sprites = new HashMap<Facing,Image>();
+		 sprites.put(Facing.RIGHT, i);
+		 sprites.put(Facing.LEFT , i.getFlippedCopy(true, false));
+	}
+	
+	public boolean isMoving(){
+	    return moving;
+	}
+	
+	public void setMoving(boolean b){
+	    moving = b;
+	}
+	
+	//move towards x_velocity = 0
+	public void decelerate(int delta) {
+	    if(x_velocity > 0){
+	        x_velocity -= decelerationSpeed * delta;
+	        if(x_velocity < 0)
+	            x_velocity = 0;
+	    }else if(x_velocity < 0){
+	        x_velocity += decelerationSpeed * delta;
+	        if(x_velocity > 0)
+	            x_velocity = 0;
+	    }
+	}
+	
+	public void jump(){
+	    if(onGround)
+	        y_velocity = -0.4f;
+	    }
+	
+	public void moveLeft(int delta){
+	    //if we aren't already moving at maximum speed
+	    if(x_velocity > -maximumSpeed){
+	        //accelerate
+	        x_velocity -= accelerationSpeed*delta;
+	        if(x_velocity < -maximumSpeed){
+	            //and if we exceed maximum speed, set it to maximum speed
+	            x_velocity = -maximumSpeed;
+	        }
+	    }
+	    moving = true;
+	    facing = Facing.LEFT;
+	}
+	
+	public void moveRight(int delta){
+	    if(x_velocity < maximumSpeed){
+	        x_velocity += accelerationSpeed*delta;
+	        if(x_velocity > maximumSpeed){
+	            x_velocity = maximumSpeed;
+	        }
+	    }
+	    moving = true;
+	    facing = Facing.RIGHT;
+	}
+	
+	
+	
     public void render(){
-    	
-      	sprite.draw(x+500,y+250);
- 
-}
+    	 //draw a moving animation if element has moved within the last 150 milliseconds
+    	if(movingAnimations != null && moving){
+    		movingAnimations.get(facing).draw(x-2,y-2);
+    	}else{
+    		sprites.get(facing).draw(x-2, y-2);
+    	}
+    	//sprite.draw(x+500,y+250);
+    }
 	
 	
 //	public void moveRight(){
--- src/game/elements/Player.java
+++ src/game/elements/Player.java
@@ -4,23 +4,28 @@ import game.Physics.*;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
 
+
 public class Player extends Element {
 
     public Player(float x, float y) throws SlickException{
     	super(x,y);
-    	sprite = new Image("images/mario_small.png");
+    	
+    	setSprite(new Image("data/img/characters/player/player.png"));
+    	 
+    	setMovingAnimation(new Image[]{new Image("data/img/characters/player/player_1.png"),new Image("data/img/characters/player/player_2.png"),
+    	new Image("data/img/characters/player/player_3.png"),new Image("data/img/characters/player/player_4.png")}
+    	,125);
     	
     	boundingShape = new AABoundingRect(x+3, y, 26, 32);
     	 
-         accelerationSpeed = 0.001f;
-         maximumSpeed = 0.15f;
-         maximumFallSpeed = 0.3f;
-         decelerationSpeed = 0.001f;
-     }
+        accelerationSpeed = 0.001f;
+        maximumSpeed = 0.15f;
+        maximumFallSpeed = 0.3f;
+        decelerationSpeed = 0.001f;
+    }
     
     public void updateBoundingShape(){
         boundingShape.updatePosition(x+3,y);
     }
     
-
 }
